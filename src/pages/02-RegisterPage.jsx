52fc5a51d8b8ba7813c365c1313e369eb6d66025
Monocress <doruk.tokinan@gmail.com>
2023-09-17 21:09:08 +0300

    Register metod frontend/backend bağlatısı

--- src/pages/02-RegisterPage.jsx
+++ src/pages/02-RegisterPage.jsx
@@ -42,6 +42,43 @@ function SelectRole({ setRole }) {
 }
 
 function RegisterCompanyManager() {
+    const defUser = {
+
+        firstname: "",
+        lastname: "",
+        password: "",
+        passwordControl: "",
+        email: "",
+        companyName: "",
+        taxNo: ""
+
+    }
+    const [user, setUser] = useState({...defUser});
+
+    function handleChange(e) {
+        setUser({ ...user, [e.target.name]: e.target.value });
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        setUser({ ...user, role: "MANAGER" });
+        console.log(user)
+        fetch("http://localhost:80/auth/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user)
+        }).then(resp => {
+            if(!resp.ok)
+           throw new Error("Hata initiate");
+            return resp.json();
+        }).then(data => {
+             setUser({...defUser})
+            console.log(data);
+        }).catch(err => console.log(err))
+
+    }
     return (
         <div className="company d-flex flex-row justify-content-center align-items-center">
             <section className="section-one w-50 d-flex flex-column justify-content-center  text-center  p-3">
@@ -53,36 +90,40 @@ function RegisterCompanyManager() {
 
             <section className="section-two w-50 d-flex flex-column justify-content-center p-4">
 
-                <form action="" className="d-flex flex-column gap-2 align-items-center px-5">
+                <form action="" className="d-flex flex-column gap-2 align-items-center px-5"onSubmit={handleSubmit}>
                     <NavLink to="/">
                         <img className="logo" src="/img/ikolay-logo.svg" alt="ikolay logo" />
                     </NavLink>
                     <label className="d-flex flex-column" htmlFor="companyName">
                         Şirket Adı
-                        <input id="companyName" type="text" name="companyName" />
+                        <input className="px-3" id="companyName" type="text" name="companyName" onChange={handleChange} value={user.companyName}/>
                     </label>
-                    <label className="d-flex flex-column" htmlFor="taxId">
-                        Vegi No
-                        <input id="taxId" type="number" name="taxId" />
+                    <label className="d-flex flex-column" htmlFor="taxNo">
+                        Vergi No
+                        <input className="px-3"  value={user.taxNo} id="taxNo" type="number" name="taxNo" onChange={handleChange} />
                     </label>
-                    <label className="d-flex flex-column" htmlFor="officialName">
-                        Yetkili Adı Soyadı
-                        <input id="officialName" type="text" name="officialName" />
+                    <label className="d-flex flex-column" htmlFor="firstname">
+                        Yetkili Adı
+                        <input className="px-3"  value={user.firstname} id="firstname" type="text" name="firstname" onChange={handleChange} />
                     </label>
-                    <label className="d-flex flex-column" htmlFor="companyEmail">
+                    <label className="d-flex flex-column" htmlFor="lastname">
+                        Yetkili Soyadı
+                        <input className="px-3"   value={user.lastname} id="lastname" type="text" name="lastname" onChange={handleChange} />
+                    </label>
+                    <label className="d-flex flex-column" htmlFor="email">
                         Şirket Eposta
-                        <input id="companyEmail" type="email" name="companyEmail" />
+                        <input className="px-3"   value={user.email}id="email" type="email" name="email" onChange={handleChange} />
                     </label>
                     <label className="d-flex flex-column" htmlFor="password">
                         Şifre
-                        <input id="password" type="password" name="password" />
+                        <input className={`${user.password != user.passwordControl && "border-danger"} px-3`}  value={user.password} id="password" type="password" name="password" onChange={handleChange} />
                     </label>
                     <label className="d-flex flex-column" htmlFor="passwordControl">
                         Şifre Onayı
-                        <input id="passwordControl" type="password" name="passwordControl" />
+                        <input className={`${user.password != user.passwordControl && "border-danger"} px-3`}  value={user.passwordControl} id="passwordControl" type="password" name="passwordControl" onChange={handleChange} />
                     </label>
                     <div className="d-flex mt-2">
-                        <button className="btn btn-lg btn-outline-primary me-2" type="button">GÖNDER</button>
+                        <button className="btn btn-lg btn-outline-primary me-2" disabled={user.email==""&&true}  type="submit">GÖNDER</button>
                         <a href="http://localhost:5173/register">
                             <button className="btn btn-lg btn-outline-secondary w-100" type="button">Vazgeç</button>
                         </a>
@@ -96,34 +137,75 @@ function RegisterCompanyManager() {
 
 }
 
+
+
+
+
 function RegisterGuest() {
+    const defUser = {
+
+        firstname: "",
+        lastname: "",
+        password: "",
+        passwordControl:"",
+        email: ""
+
+    }
+    const [user, setUser] = useState({...defUser});
+   
+    function handleChange(e) {
+        setUser({ ...user, [e.target.name]: e.target.value });
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        setUser({ ...user, role: "VISITOR" });
+        console.log(user)
+        fetch("http://localhost:80/auth/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user)
+        }).then(resp => {
+            if(resp.ok)
+            setUser({...defUser});
+            return resp.json();
+        }).then(data => console.log(data)).catch(err => console.log(err))
+        
+    }
+   
     return (
         <div className="guest d-flex flex-row justify-content-center align-items-center">
 
             <section className="section-two w-50 d-flex flex-column justify-content-center p-4" style={{ borderRight: "1px solid #003C6B" }}>
 
-                <form action="" className="d-flex flex-column align-items-center gap-2 px-5">
+                <form action="" className="d-flex flex-column align-items-center gap-2 px-5" onSubmit={handleSubmit}>
                     <NavLink to="/">
                         <img className="logo" src="/img/ikolay-logo.svg" alt="ikolay logo" />
                     </NavLink>
-                    <label className="d-flex flex-column" htmlFor="guestFullname">
-                        Ad Soyad
-                        <input id="guestFullname" type="text" name="guestFullname" />
+                    <label className="d-flex flex-column" htmlFor="firstname">
+                        Ad
+                        <input className="px-3" id="firstname" type="text" value={user.firstname} name="firstname" onChange={handleChange} />
                     </label>
-                    <label className="d-flex flex-column" htmlFor="guestEmail">
+                    <label className="d-flex flex-column" htmlFor="lastname" >
+                        Soyad
+                        <input className="px-3" id="lastname" type="text" value={user.lastname} name="lastname" onChange={handleChange} />
+                    </label>
+                    <label className="d-flex flex-column" htmlFor="email">
                         Eposta
-                        <input id="guestEmail" type="email" name="guestEmail" />
+                        <input className="px-3"  id="email" type="email" value={user.email} name="email" onChange={handleChange} />
                     </label>
                     <label className="d-flex flex-column" htmlFor="password">
                         Şifre
-                        <input id="password" type="password" name="password" />
+                        <input className={`${user.password != user.passwordControl && "border-danger"} px-3`} id="password" value={user.password} type="password" name="password" onChange={handleChange} />
                     </label>
                     <label className="d-flex flex-column" htmlFor="passwordControl">
                         Şifre Onayı
-                        <input id="passwordControl" type="password" name="passwordControl" />
+                        <input className={`${user.password != user.passwordControl && "border-danger"} px-3`} id="passwordControl" value={user.passwordControl} type="password" name="passwordControl" onChange={handleChange} />
                     </label>
                     <div className="d-flex mt-2">
-                        <button className="btn btn-lg btn-outline-primary me-2" type="button">GÖNDER</button>
+                        <button className="btn btn-lg btn-outline-primary me-2" disabled={user.email==""&&true}  type="submit">GÖNDER</button>
                         <a href="http://localhost:5173/register">
                             <button className="btn btn-lg btn-outline-secondary w-100" type="button">Vazgeç</button>
                         </a>
