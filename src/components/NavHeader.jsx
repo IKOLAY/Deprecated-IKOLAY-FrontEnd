b2591451e37feeb372e7a37681b3f07cba0d87f6
Hulya Martli aka. Guerillagi <66822245+HulyaMartli@users.noreply.github.com>
2023-09-25 09:11:28 +0300

    search button style

--- src/components/NavHeader.jsx
+++ src/components/NavHeader.jsx
@@ -48,7 +48,7 @@ export default function NavHeader() {
                             placeholder="Şirket ara"
                             aria-label="Search"
                         />
-                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+                        <button className="btn btn-outline-primary my-2 my-sm-0" type="submit">
                             Ara
                         </button>
                     </form>
