9517b028a3606ed426aedf1a732c11214874d85a
Hulya Martli aka. Guerillagi <66822245+HulyaMartli@users.noreply.github.com>
2023-09-28 18:37:04 +0300

    admin page logout functionality

--- src/pages/05-AdminPage.jsx
+++ src/pages/05-AdminPage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import "../assets/styles/AdminPage.css"
+import { NavLink } from "react-bootstrap";
 
 export default function AdminPage() {
     const [section, setSection] = useState(null)
@@ -14,6 +15,8 @@ export default function AdminPage() {
         e.preventDefault();
         setSection(e.target.name)
     }
+
+
 
     return (
         <>
@@ -38,7 +41,7 @@ export default function AdminPage() {
 
                         <section style={{ height: "85vh", backgroundColor: "white" }} className="d-flex flex-column align-items-start p-2">
                             {section === "register-requests" && confirmInfo.map(companyInfo => <RegisterRequest key={companyInfo.companyId} {...companyInfo} />)}
-                            {section === "confirm-comments" && <AcceptOrRejectComments/>}
+                            {section === "confirm-comments" && <AcceptOrRejectComments />}
                         </section>
                     </div>
                 </div>
@@ -49,10 +52,17 @@ export default function AdminPage() {
 }
 
 function AdminHeader() {
+    function handleLogout(e) {
+        window.localStorage.clear("token")
+        window.localStorage.clear("user")
+    }
+
     return (
         <header className="px-4">
             <nav className="navbar w-100" style={{ height: "15vh" }}>
-                <img src="/img/ikolay-admin.svg" alt="ikolay-admin logo" />
+                <a href="/">
+                    <img src="/img/ikolay-admin.svg" alt="ikolay-admin logo" />
+                </a>
                 <div className="dropdown">
                     <a
                         href="#"
@@ -72,10 +82,8 @@ function AdminHeader() {
                     </a>
                     <ul className="dropdown-menu text-small shadow"
                         aria-labelledby="dropdownUser1">
-                        <li>
-                            <a className="dropdown-item" href="#">
-                                Çıkış
-                            </a>
+                        <li className="text-center">
+                            <a href="http://localhost:5173/" onClick={handleLogout}>Çıkış</a>
                         </li>
                     </ul>
                 </div>
@@ -198,134 +206,134 @@ function RegisterRequest({ companyId, email, firstname, lastname, companyName, t
     )
 }
 
-function AcceptOrRejectComments(){
-    const[pendingComments,setPendingComments] = useState([])
+function AcceptOrRejectComments() {
+    const [pendingComments, setPendingComments] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch("http://localhost/comment/findallcommentforadmin")
-        .then(resp=>resp.json())
-        .then(data=>setPendingComments(data))
-        .catch(err=>console.log(err));
-    },[])
+            .then(resp => resp.json())
+            .then(data => setPendingComments(data))
+            .catch(err => console.log(err));
+    }, [])
 
 
     return (
-        pendingComments.map(comment => <CommentRow key={comment.userId} {...comment} comments={pendingComments} setComments={setPendingComments}/>)
+        pendingComments.map(comment => <CommentRow key={comment.userId} {...comment} comments={pendingComments} setComments={setPendingComments} />)
     )
 }
 
-function CommentRow({id,companyId,userId,content,comments,setComments}){
-    const[user, setUser] = useState({firstname:"",lastname:""});
-    const[company, setCompany] = useState({companyName:"",taxNo:""});
-    const[toggle,setToggle] = useState(true);
+function CommentRow({ id, companyId, userId, content, comments, setComments }) {
+    const [user, setUser] = useState({ firstname: "", lastname: "" });
+    const [company, setCompany] = useState({ companyName: "", taxNo: "" });
+    const [toggle, setToggle] = useState(true);
 
-    
 
-    function handleClick(e){
-        if(e.target.name=="accept"){
+
+    function handleClick(e) {
+        if (e.target.name == "accept") {
             fetch(`http://localhost/comment/acceptcomment/${id}`).then(resp => {
                 if (resp.ok) {
-                    setComments(comments.filter(comment => comment.id!=id));
+                    setComments(comments.filter(comment => comment.id != id));
                 }
             }).catch(err => console.log(err))
-        } else{
+        } else {
             fetch(`http://localhost/comment/rejectcomment/${id}`).then(resp => {
                 if (resp.ok) {
-                    setComments(comments.filter(comment => comment.id!=id));
+                    setComments(comments.filter(comment => comment.id != id));
                 }
             }).catch(err => console.log(err))
         }
     }
-    function handleToggle(e){
-      setToggle(false)
+    function handleToggle(e) {
+        setToggle(false)
     }
     return (
-    <div className="border border-primary rounded p-3 w-100 d-flex justify-content-between align-items-center">
-    <span style={{ color: "black" }}>{content}</span>
-    <div className="d-flex gap-2">
-    <button className="btn btn-outline-primary" onClick={handleClick} name="accept">Onayla</button>
-    <button className="btn btn-outline-danger" onClick={handleClick} name="reject">Reddet</button>
-        <button type="button"
-            className="btn btn-outline-secondary"
-            data-bs-toggle="modal"
-            data-bs-target={`#modalAdd${userId}`}
-            onClick={handleToggle}
-            >Detay</button>
-        <div
-            className="modal fade"
-            id={`modalAdd${userId}`}
-            aria-labelledby="exampleModalLabel"
-            aria-hidden="true"
-        >
-            <div className="modal-dialog">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <h1 style={{ color: "black" }} className="modal-title fs-5" id="exampleModalLabel">
-                            Kullanıcı ve Firma Detayları:
-                        </h1>
-                        <button
-                            type="button"
-                            className="btn-close"
-                            data-bs-dismiss="modal"
-                            aria-label="Close"
-                        />
-                    </div>
-                    <div className="modal-body">
-                        <form typeof="submit">
-                            <div className="form-group">
-                                {toggle ? <label >Kullanici yükleniyor.</label> :<GetFirstAndLastName userId={userId} user={user} setUser={setUser}/>}
-                                {toggle ? <label >Firma yükleniyor.</label> :<GetCompanyNameAndTaxNo companyId={companyId} company={company} setCompany={setCompany} />}
-                                
-                            </div>
-                            <div className="modal-footer justify-content-between">
+        <div className="border border-primary rounded p-3 w-100 d-flex justify-content-between align-items-center">
+            <span style={{ color: "black" }}>{content}</span>
+            <div className="d-flex gap-2">
+                <button className="btn btn-outline-primary" onClick={handleClick} name="accept">Onayla</button>
+                <button className="btn btn-outline-danger" onClick={handleClick} name="reject">Reddet</button>
+                <button type="button"
+                    className="btn btn-outline-secondary"
+                    data-bs-toggle="modal"
+                    data-bs-target={`#modalAdd${userId}`}
+                    onClick={handleToggle}
+                >Detay</button>
+                <div
+                    className="modal fade"
+                    id={`modalAdd${userId}`}
+                    aria-labelledby="exampleModalLabel"
+                    aria-hidden="true"
+                >
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h1 style={{ color: "black" }} className="modal-title fs-5" id="exampleModalLabel">
+                                    Kullanıcı ve Firma Detayları:
+                                </h1>
                                 <button
                                     type="button"
-                                    className="btn btn-secondary"
+                                    className="btn-close"
                                     data-bs-dismiss="modal"
-                                >
-                                    Kapat
-                                </button>
-                            
+                                    aria-label="Close"
+                                />
                             </div>
-                        </form>
-                    </div>
+                            <div className="modal-body">
+                                <form typeof="submit">
+                                    <div className="form-group">
+                                        {toggle ? <label >Kullanici yükleniyor.</label> : <GetFirstAndLastName userId={userId} user={user} setUser={setUser} />}
+                                        {toggle ? <label >Firma yükleniyor.</label> : <GetCompanyNameAndTaxNo companyId={companyId} company={company} setCompany={setCompany} />}
 
+                                    </div>
+                                    <div className="modal-footer justify-content-between">
+                                        <button
+                                            type="button"
+                                            className="btn btn-secondary"
+                                            data-bs-dismiss="modal"
+                                        >
+                                            Kapat
+                                        </button>
+
+                                    </div>
+                                </form>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
-)
+    )
 
 }
 
 
-function GetFirstAndLastName({userId,user,setUser}){
-    useEffect(()=>{
-        fetch(`http://localhost/user/getusersfirstandlastname/${userId}`).then(resp => resp.json()).then(data=>{
-        if(!data.firstname)
-        throw new Error(data.message);
-        setUser(data);
-    }).catch(err => console.log(err))
-    },[])
+function GetFirstAndLastName({ userId, user, setUser }) {
+    useEffect(() => {
+        fetch(`http://localhost/user/getusersfirstandlastname/${userId}`).then(resp => resp.json()).then(data => {
+            if (!data.firstname)
+                throw new Error(data.message);
+            setUser(data);
+        }).catch(err => console.log(err))
+    }, [])
 
-    return<>
-    <label>Kullanici Ad Soyad</label>
-    <label>{user.firstname} {user.lastname}</label>
+    return <>
+        <label>Kullanici Ad Soyad</label>
+        <label>{user.firstname} {user.lastname}</label>
     </>
 }
 
-function GetCompanyNameAndTaxNo({companyId,company,setCompany}){
-useEffect(()=>{
-        fetch(`http://localhost/company/companyinformation?id=${companyId}`).then(resp => resp.json()).then(data=>{
-        if(!data.taxNo)
-        throw new Error(data.message);
-        setCompany(data);
-    })
-    },[]);
-   
+function GetCompanyNameAndTaxNo({ companyId, company, setCompany }) {
+    useEffect(() => {
+        fetch(`http://localhost/company/companyinformation?id=${companyId}`).then(resp => resp.json()).then(data => {
+            if (!data.taxNo)
+                throw new Error(data.message);
+            setCompany(data);
+        })
+    }, []);
+
 
     return <><label>Firma Adı ve Vergi Numarası</label>
-    <label>{company.companyName} {company.taxNo}</label></>
+        <label>{company.companyName} {company.taxNo}</label></>
 
 }
